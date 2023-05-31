fda8dd39af27e66272c71ef2807103e6b868d001
wiwi <legoupil.william@gmail.com>
2023-05-31 14:55:12 +0200

    Animation

--- Program.cs
+++ Program.cs
@@ -9,11 +9,28 @@ Console.WriteLine(de);
 // for taille max :
 // plateau.add(aleatoire(1ou2ou3)) 1 neutre ou 2 bonnus ou 3 malus (4 autres serpent , echelle))
 
+Boolean trouve = true;
+while (trouve) { 
 
-while (true) { }
+traitementde(de);
 
- /*J1 propose lancé dés
+trouve = false;
+};
 
- J2*/
 
 
+void traitementde (int aleatoire){
+String[] tableau = new String[6] {"[ 1 ]","( 2 )","[ 3 ]","( 4 )","[ 5 ]","( 6 )"};
+int controle = 0;
+for(int i = 0; i < 40; i++)
+{
+    Console.Clear();
+    Console.WriteLine(tableau[controle]);
+    Thread.Sleep(100);
+    if(controle == 5){controle =-1;}
+    controle++;
+}
+    Console.Clear();
+    Console.WriteLine("[ "+aleatoire+" ]");
+
+}
