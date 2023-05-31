35a1f22185e786b59e9c4e6e37147a105ac7f66d
wiwi <legoupil.william@gmail.com>
2023-05-31 15:09:07 +0200

    Anim methode

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
 ﻿Random aleatoire = new Random();
-
-int min = 0;
-int max ;
-int de = aleatoire.Next(1, 6);
-Console.WriteLine(de);
-// tableau plateau
-// generation case
-// for taille max :
-// plateau.add(aleatoire(1ou2ou3)) 1 neutre ou 2 bonnus ou 3 malus (4 autres serpent , echelle))
-
 Boolean trouve = true;
-while (trouve) { 
 
+while (true) { 
+
+Console.WriteLine("Appuyer sur la touche entrer pour Lancer le dé");
+String choix = Console.ReadLine();
+int de = aleatoire.Next(1, 7);
 traitementde(de);
 
-trouve = false;
+
+// trouve = false;
 };
 
 
